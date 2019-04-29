6e7b2c5e9d4260ae6219eb11c5a0cadc59d9f27a
Chris Lam <hehachris@users.noreply.github.com>
2019-04-29 12:06:56 +0800

    Comment out cask tapping

--- general_mac.sh
+++ general_mac.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-CASKS="franz google-chrome"
+CASKS="franz google-chrome "
 
 echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Installing Homebrew Cask..."
-brew tap caskroom/cask
+# echo "Installing Homebrew Cask..."
+# brew tap caskroom/cask
 
 echo "Installing Casks..."
 brew cask install $CASKS
