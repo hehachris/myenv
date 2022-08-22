0e4d3a29abefa50d605182a37b82fac6da4145dc
Chris Lam <chris@spaceshipapp.com>
2022-08-22 22:45:45 +0800

    Update cask install cmd

--- mac.sh
+++ mac.sh
@@ -27,10 +27,10 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/chrislam/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 $(ask "general software ($GENERAL_CASKS)") \
-    && brew cask install $GENERAL_CASKS
+    && brew install --cask $GENERAL_CASKS
 
 $(ask "dev tools ($DEV_CASKS)") \
-    && brew cask install $DEV_CASKS
+    && brew install --cask $DEV_CASKS
 
 $(ask "my casks colletion ($MY_CASKS)") \
-    && brew cask install $MY_CASKS
+    && brew install --cask $MY_CASKS
